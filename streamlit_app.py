16d9c8d3c22c2d70029a7aa6e1918fce75373b62
Mukundh J <93877999+Mukundh0007@users.noreply.github.com>
2025-03-20 08:16:02 +0000

    properly working code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,30 +3,79 @@ import streamlit as st
 from groq import Groq
 import subprocess
 import os
+import time
+import atexit
+
+def remove_pjt():
+    if os.path.exists(f"{cwd}/PJTmain"):
+        subprocess.run(["rm", "-rf", f"{cwd}/PJTmain"])
+    return
+def prompt_and_update():
+    global system_instruction, client, cwd, vm_ip, repo_url
+    if document and prompt:
+        # Process the uploaded file and prompt.
+        messages = [
+            {
+                "role": "system",
+                "content": system_instruction
+            },
+            {
+                "role": "user",
+                "content": f"Original Code: {document} \n\n---\n\n User Request:{prompt}"
+            }
+        ]
+
+        # Generate an answer using the API.
+        try:
+            stream = client.chat.completions.create(
+                model="llama-3.3-70b-versatile",
+                messages=messages,
+                stream=True,
+            )
+
+            # Write the response to a test.py file.
+            with open(f"{cwd}/PJTmain/test.py", "w") as test_file:
+                for chunk in stream:
+                    content = chunk.choices[0].delta.content
+                    try:
+                        if content.strip() not in ["```python","python", "```"]:
+                            test_file.write(content)
+                    except:pass
+        except Exception as e:
+            with open(f"{cwd}/PJTmain/test.py", "w") as test_file:
+                try:
+                    test_file.write(document)
+                except:pass
+        # Push the test.py file to the GitHub repository.
+
+        subprocess.run(["git", "init", f"{cwd}/PJTmain"])
+        subprocess.run(["git", "-C", f"{cwd}/PJTmain", "remote", "set-url", "origin", repo_url])
+        subprocess.run(["git", "-C", f"{cwd}/PJTmain", "config", "pull.rebase", "false"])  # Set pull strategy to merge
+        subprocess.run(["git", "-C", f"{cwd}/PJTmain", "pull", "origin", "main"])  # Pull changes from the remote repository
+        subprocess.run(["git", "-C", f"{cwd}/PJTmain", "add", "test.py"])
+        subprocess.run(["git", "-C", f"{cwd}/PJTmain", "commit", "-m", "Add test.py"])
+        subprocess.run(["git", "-C", f"{cwd}/PJTmain", "push", "-u", "origin", "main", "--force"])  # Force push to the remote repository
+        
+        # Send a curl request to the specified URL.
+        subprocess.run([ "curl", f"http://{vm_ip}/update"])
+        st.write("The code has been modified based on your request. Please check the server.")
 
 # Get the current working directory
 cwd = os.getcwd()
-
+vm_ip = st.secrets["VM_IP"]
 # Show title and description.
 st.title("📄 AI Software Manager")
-# st.write(
-#     "Upload a document below and ask a question about it – GPT will answer! "
-#     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-# )
-
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-#openai_api_key = st.text_input("OpenAI API Key", type="password")
-
-    # Create an OpenAI client.
+system_instruction = (
+            "Modify the given Python code based on the user's instruction. Ensure that all necessary changes are made and provide appropriate comments on the modified lines. Return the entire modified code as plain text without any additional explanations or omissions."
+        )
+# Create an AI client.
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 client = Groq(api_key=GROQ_API_KEY) # Replace with your OpenAI API key
 PAT = st.secrets["GitPAT"]
 repo_url = f"https://{PAT}@github.com/Mukundh0007/PJTmain.git"
-if os.path.exists(f"{cwd}/PJTmain"):
-    subprocess.run(["rm", "-rf", f"{cwd}/PJTmain"])
-subprocess.run(["git", "clone", repo_url, f"{cwd}/PJTmain"])
+
+if not os.path.exists(f"{cwd}/PJTmain"):
+    subprocess.run(["git", "clone", repo_url, f"{cwd}/PJTmain"])
 
 # Use the specified file instead of file uploader
 file_path = f"{cwd}/PJTmain/codeV2.py"
@@ -34,56 +83,14 @@ with open(file_path, "r") as file:
     document = file.read()
 
 # Ask the user for a prompt via `st.text_area`.
-prompt = st.text_area(
-    "What can I assist you with?",
-    placeholder="Change the button color to...., Shift position of the image...",
-    disabled=not document,
-)
-
-if document and prompt:
-    system_instruction = (
-        "Modify the given Python code based on the user's instruction. Ensure that all necessary changes are made and provide appropriate comments on the modified lines. Return the entire modified code as plain text without any additional explanations or omissions."
+with st.form(key='prompt_form', clear_on_submit=True):
+    prompt = st.text_input(
+        "What can I assist you with?",
+        placeholder="Change the font color to....",
+        disabled=not document,
     )
-    # Process the uploaded file and prompt.
-    messages = [
-        {
-            "role": "system",
-            "content": system_instruction
-        },
-        {
-            "role": "user",
-            "content": f"Original Code: {document} \n\n---\n\n User Request:{prompt}"
-        }
-    ]
+    submit_button = st.form_submit_button(label='Submit',use_container_width=True)
+    if submit_button:
+        prompt_and_update()
 
-    # Generate an answer using the OpenAI API.
-    stream = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=messages,
-        stream=True,
-    )
-
-    # Write the response to a test.py file.
-    with open(f"{cwd}/PJTmain/test.py", "w") as test_file:
-        for chunk in stream:
-            content = chunk.choices[0].delta.content
-            try:
-                if content.strip() not in ["```python","python", "```"]:
-                    test_file.write(content)
-            except:pass
-
-    # Push the test.py file to the GitHub repository.
-
-    subprocess.run(["git", "init", f"{cwd}/PJTmain"])
-    subprocess.run(["git", "-C", f"{cwd}/PJTmain", "remote", "set-url", "origin", repo_url])
-    subprocess.run(["git", "-C", f"{cwd}/PJTmain", "config", "pull.rebase", "false"])  # Set pull strategy to merge
-    subprocess.run(["git", "-C", f"{cwd}/PJTmain", "pull", "origin", "main"])  # Pull changes from the remote repository
-    subprocess.run(["git", "-C", f"{cwd}/PJTmain", "add", "test.py"])
-    subprocess.run(["git", "-C", f"{cwd}/PJTmain", "commit", "-m", "Add test.py"])
-    subprocess.run(["git", "-C", f"{cwd}/PJTmain", "push", "-u", "origin", "main", "--force"])  # Force push to the remote repository
-    
-    # Send a curl request to the specified URL.
-    # subprocess.run([
-    #     "curl", "-X", "POST", "http://98.70.35.30:5000/update"
-    # ])
-    st.write("The code has been modified based on your request. Please check the server.")
+atexit.register(remove_pjt)
